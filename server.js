96efe19a114f7bbeeddb4e6b10b7c5452c29eb6f
radash1 <rdsliyanage@gmail.com>
2021-08-19 21:04:31 +0530

    Boilerplate code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} => ${req.method+' '+req.originalUrl}`);
+  next();
+});
+
+app.get('/', async (req, res) => {
+  res.send({
+    'message': 'Welcome to the GeniusBit api.',
+  });
+});
+
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, function() {
+  console.log('Server listening on port '+process.env.PORT);
+});
