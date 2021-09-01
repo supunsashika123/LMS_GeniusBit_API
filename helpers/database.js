40050f34f67b0a772a9963333b12716c72759de0
supunsashika123 <supun.eternal@gmail.com>
2021-09-01 21:25:25 +0530

    Db update

--- helpers/database.js
+++ helpers/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://GeniusBit1:Nibm2021@zone1.v4dpd.mongodb.net/geniusbit', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://admin:geniusbit2021@104.42.249.174:27017/geniusbit?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 module.exports = {
