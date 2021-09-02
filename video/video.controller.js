8cd090e15c16ab81f1e33d850fa494bc168f98d5
radash1 <rdsliyanage@gmail.com>
2021-09-02 22:02:40 +0530

    get-videos

--- video/video.controller.js
+++ video/video.controller.js
@@ -1,4 +1,5 @@
 router.post('/', authenticateToken, adminOnly, create);
+router.get('/getFiltered', authenticateToken, getFiltered);
 
 async function create(req, res) {
     let new_video = req.body;
@@ -23,4 +24,55 @@ async function create(req, res) {
     let updated_video = await videoService.update(created_video, created_video._id);
 
     return res.json(success("Video has been created.", updated_video))
+}
+
+async function getFiltered(req, res) {
+  let filters = req.query;
+
+  let status = filters.status;
+
+  if (req.user.type === 'student') {
+      status = 'active';
+  }
+
+  if (status) {
+      delete filters.status;
+      const todayDate = new Date();
+      todayDate.setHours(0);
+      todayDate.setMinutes(0);
+      todayDate.setSeconds(0);
+      todayDate.setMilliseconds(0);
+      todayDate.setDate(todayDate.getDate() + 1)
+      if (status === 'active') {
+          filters.$where = todayDate.getTime() + " >= this.auto_publish_date.getTime() && this.expiry_date.getTime() > " + todayDate.getTime() + " ";
+      } else if (status === 'expired') {
+          filters.$where = todayDate.getTime() + " >= this.expiry_date.getTime() ";
+      } else if (status === 'pending') {
+          filters.$where = todayDate.getTime() + " < this.auto_publish_date.getTime() ";
+      } else {
+          return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed('unidentified status.'));
+      }
+  }
+
+  if (filters['class_id']) {
+      const class_id = filters['class_id'];
+      delete filters['class_id'];
+      const exp = "this.classes.includes('" + class_id + "')";
+      console.log(exp)
+      if (filters.$where) {
+          filters.$where += "&& " + exp;
+      } else {
+          filters.$where = exp;
+      }
+  }
+
+  console.log(filters)
+
+  let videos = await videoService.getAll(filters, {
+      title: true,
+      description: true,
+      thumbnail: true,
+      url: true,
+  });
+  return res.json(success(videos.length === 0 ? 'no videos found for provided criteria' : "videos queried.", videos))
 }
