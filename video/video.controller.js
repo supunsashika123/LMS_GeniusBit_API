c2a1b6c1462469fe1454b22580517145f9b8a40c
radash1 <rdsliyanage@gmail.com>
2021-09-03 21:49:06 +0530

    admin get all videos

--- video/video.controller.js
+++ video/video.controller.js
@@ -1,29 +1,30 @@
 router.post('/', authenticateToken, adminOnly, create);
 router.get('/getFiltered', authenticateToken, getFiltered);
 
+
 async function create(req, res) {
-    let new_video = req.body;
-    const error = await validateVideo(new_video);
-    if (error) return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed(error));
+  let new_video = req.body;
+  const error = await validateVideo(new_video);
+  if (error) return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed(error));
 
-    const base64thumbnail = new_video.thumbnail;
-    console.log(base64thumbnail)
+  const base64thumbnail = new_video.thumbnail;
+  console.log(base64thumbnail)
 
-    delete new_video.thumbnail;
+  delete new_video.thumbnail;
 
-    let created_video = await videoService.create(new_video);
-    if (created_video.error) return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed(created_video.error));
+  let created_video = await videoService.create(new_video);
+  if (created_video.error) return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed(created_video.error));
 
-    if (!base64thumbnail || base64thumbnail.trim().length === 0) return res.json(success("Video has been created.", created_video))
+  if (!base64thumbnail || base64thumbnail.trim().length === 0) return res.json(success("Video has been created.", created_video))
 
-    if (isBase64(base64thumbnail)) {
-        const created_file_name = await fs.uploadImageToFileSystem(base64thumbnail, "video/" + created_video._id);
-        created_video.thumbnail = process.env.UPLOAD_PATH + created_file_name;
-    }
+  if (isBase64(base64thumbnail)) {
+    const created_file_name = await fs.uploadImageToFileSystem(base64thumbnail, "video/" + created_video._id);
+    created_video.thumbnail = process.env.UPLOAD_PATH + created_file_name;
+  }
 
-    let updated_video = await videoService.update(created_video, created_video._id);
+  let updated_video = await videoService.update(created_video, created_video._id);
 
-    return res.json(success("Video has been created.", updated_video))
+  return res.json(success("Video has been created.", updated_video))
 }
 
 async function getFiltered(req, res) {
@@ -32,47 +33,47 @@ async function getFiltered(req, res) {
   let status = filters.status;
 
   if (req.user.type === 'student') {
-      status = 'active';
+    status = 'active';
   }
 
   if (status) {
-      delete filters.status;
-      const todayDate = new Date();
-      todayDate.setHours(0);
-      todayDate.setMinutes(0);
-      todayDate.setSeconds(0);
-      todayDate.setMilliseconds(0);
-      todayDate.setDate(todayDate.getDate() + 1)
-      if (status === 'active') {
-          filters.$where = todayDate.getTime() + " >= this.auto_publish_date.getTime() && this.expiry_date.getTime() > " + todayDate.getTime() + " ";
-      } else if (status === 'expired') {
-          filters.$where = todayDate.getTime() + " >= this.expiry_date.getTime() ";
-      } else if (status === 'pending') {
-          filters.$where = todayDate.getTime() + " < this.auto_publish_date.getTime() ";
-      } else {
-          return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed('unidentified status.'));
-      }
+    delete filters.status;
+    const todayDate = new Date();
+    todayDate.setHours(0);
+    todayDate.setMinutes(0);
+    todayDate.setSeconds(0);
+    todayDate.setMilliseconds(0);
+    todayDate.setDate(todayDate.getDate() + 1)
+    if (status === 'active') {
+      filters.$where = todayDate.getTime() + " >= this.auto_publish_date.getTime() && this.expiry_date.getTime() > " + todayDate.getTime() + " ";
+    } else if (status === 'expired') {
+      filters.$where = todayDate.getTime() + " >= this.expiry_date.getTime() ";
+    } else if (status === 'pending') {
+      filters.$where = todayDate.getTime() + " < this.auto_publish_date.getTime() ";
+    } else {
+      return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed('unidentified status.'));
+    }
   }
 
   if (filters['class_id']) {
-      const class_id = filters['class_id'];
-      delete filters['class_id'];
-      const exp = "this.classes.includes('" + class_id + "')";
-      console.log(exp)
-      if (filters.$where) {
-          filters.$where += "&& " + exp;
-      } else {
-          filters.$where = exp;
-      }
+    const class_id = filters['class_id'];
+    delete filters['class_id'];
+    const exp = "this.classes.includes('" + class_id + "')";
+    console.log(exp)
+    if (filters.$where) {
+      filters.$where += "&& " + exp;
+    } else {
+      filters.$where = exp;
+    }
   }
 
   console.log(filters)
 
   let videos = await videoService.getAll(filters, {
-      title: true,
-      description: true,
-      thumbnail: true,
-      url: true,
+    title: true,
+    description: true,
+    thumbnail: true,
+    url: true,
   });
   return res.json(success(videos.length === 0 ? 'no videos found for provided criteria' : "videos queried.", videos))
 }
--- video/video.service.js
+++ video/video.service.js
@@ -1,32 +1,32 @@
-async function create(_video){
-    _video.created_date = (new Date()).toISOString().split('T')[0];
+async function create(_video) {
+  _video.created_date = (new Date()).toISOString().split('T')[0];
 
-    const new_video = Video(_video);
-    let response = {};
-    try{
-        response = await new_video.save();
-    }catch (err){
-        console.log(err);
-        response.error = err._message;
-    }
-    return response;
+  const new_video = Video(_video);
+  let response = {};
+  try {
+    response = await new_video.save();
+  } catch (err) {
+    console.log(err);
+    response.error = err._message;
+  }
+  return response;
 }
 
-async function update(user,id){
-    const found_video = await Video.findOne({_id: id});
+async function update(user, id) {
+  const found_video = await Video.findOne({ _id: id });
 
-    Object.assign(found_video, user);
+  Object.assign(found_video, user);
 
-    let response = {};
-    try{
-        response = await found_video.save();
-    }catch (err){
-        console.log(err)
-        response.error = "There was an issue while updating the user.";
-    }
-    return response;
+  let response = {};
+  try {
+    response = await found_video.save();
+  } catch (err) {
+    console.log(err)
+    response.error = "There was an issue while updating the user.";
+  }
+  return response;
 }
 
-async function getAll(filter = {}, project = {}){
-  return Video.find(filter, project).sort({_id: -1});
+async function getAll(filter = {}, project = {}) {
+  return Video.find(filter, project).sort({ _id: -1 });
 }
