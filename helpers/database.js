68d92fb5579677857ba9526ed6c228660cf7bba8
supunsashika123 <supun.eternal@gmail.com>
2021-09-09 21:26:35 +0530

    db fix

--- helpers/database.js
+++ helpers/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:geniusbit2021@104.42.249.174:27017/geniusbit?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_PATH, {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 module.exports = {
