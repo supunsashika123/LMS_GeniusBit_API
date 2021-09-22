4485edbe126d969d4aee06a46f78be00c7fdd09a
supunsashika123 <supun.eternal@gmail.com>
2021-09-22 21:04:41 +0530

    doc fixes

--- doc/doc.controller.js
+++ doc/doc.controller.js
@@ -12,8 +12,11 @@ const docService = require('./doc.service');
 
 const router = express.Router();
 
-router.post('/', authenticateToken, adminOnly, create);
+router.get('/getFiltered', authenticateToken, getFiltered);
 router.get('/:id', authenticateToken, getById);
+router.post('/', authenticateToken, adminOnly, create);
+
+router.put('/:id', authenticateToken, adminOnly, update);
 
 module.exports = router;
 
@@ -47,8 +50,60 @@ async function create(req, res) {
     return res.json(success('Doc has been created.', updated_doc ? updated_doc : created_doc));
   });
 }
+
 async function getById(req, res) {
   const doc = await docService.getById(req.params.id);
   if (!doc) return res.status(Codes.BAD_REQUEST).json(failed('Doc with id provided does not exists.'));
   return res.json(success('Doc queried.', doc));
+}
+
+async function update(req, res) {
+  const doc = req.body;
+  const id = req.params.id;
+
+  if (!isMongoId(id)) return res.status(Codes.BAD_REQUEST).json(failed('invalid mongoId.'));
+
+  const error = await validateDocs(doc);
+  if (error) return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed(error));
+
+  const found_doc = await docService.getById(id);
+  if (!found_doc) return res.status(Codes.NOT_FOUND).json(failed('provided docid is not valid.'));
+
+  const updated_doc = await docService.update(doc, id);
+  return res.json(success('Doc has been updated.', updated_doc));
+}
+
+async function getFiltered(req, res) {
+  const filters = req.query;
+
+  let status = filters.status;
+
+  if (req.user.type === 'student') {
+    status = 'active';
+  }
+
+  delete filters.status;
+  const todayDate = new Date();
+
+  todayDate.setHours(0);
+  todayDate.setMinutes(0);
+  todayDate.setSeconds(0);
+  todayDate.setMilliseconds(0);
+  todayDate.setDate(todayDate.getDate() + 1);
+  if (status === 'active') {
+    filters.$where = todayDate.getTime() + ' >= this.auto_publish_date.getTime() && this.expiry_date.getTime() > ' + todayDate.getTime();
+  } else if (status === 'expired') {
+    filters.$where = todayDate.getTime() + ' >= this.expiry_date.getTime() ';
+  } else if (status === 'pending') {
+    filters.$where = todayDate.getTime() + ' < this.auto_publish_date.getTime() ';
+  } else {
+    return res.status(Codes.INTERNAL_SERVER_ERROR).json(failed('unidentified status.'));
+  }
+
+  const videos = await docService.getAll(filters, {
+    title: true,
+    description: true,
+    url: true,
+  });
+  return res.json(success(videos.length === 0 ? 'no doc found for provided criteria' : 'doc queried.', videos));
 }
--- doc/doc.service.js
+++ doc/doc.service.js
@@ -2,25 +2,48 @@ const db = require('../helpers/database');
 const Doc = db.Doc;
 
 module.exports = {
-    create,
-    getById,
-    getAll,
-    update
+  create,
+  getById,
+  getAll,
+  update,
+};
+
+async function create(_class) {
+  _class.created_date = (new Date()).toISOString().split('T')[0];
+
+  const new_class = Doc(_class);
+  let response = {};
+  try {
+    response = await new_class.save();
+  } catch (err) {
+    console.log(err);
+    response.error = 'There was an issue while creating the doc.';
+  }
+  return response;
 }
 
-async function create(_class){
-    _class.created_date = (new Date()).toISOString().split('T')[0];
-
-    const new_class = Doc(_class);
-    let response = {};
-    try{
-        response = await new_class.save();
-    }catch (err){
-        console.log(err)
-        response.error = "There was an issue while creating the doc.";
-    }
-    return response;
-}
-async function getById(id){
+async function getById(id) {
   return Doc.findOne({_id: new Object(id), deleted: false});
+}
+
+async function getAll(filter = {}, project = {}) {
+  return Doc.find({
+    deleted: false,
+    ...filter,
+  }, project).sort({_id: -1});
+}
+
+async function update(doc, id) {
+  const found_doc = await Doc.findOne({_id: id});
+
+  Object.assign(found_doc, doc);
+
+  let response = {};
+  try {
+    response = await found_doc.save();
+  } catch (err) {
+    console.log(err);
+    response.error = 'There was an issue while updating the doc.';
+  }
+  return response;
 }
